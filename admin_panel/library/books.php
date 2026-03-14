a1b0ba3b11f15ddf8f099e29f815cd074ba86552
limbanpembe-creator <limbanpembe@gmail.com>
2026-03-14 10:17:13 +0200

    Create books.php for library module

---
+++ admin_panel/library/books.php
@@ -0,0 +1,16 @@
+<?php
+// books.php
+
+// Functionality to manage books in the library
+function addBook($title, $author, $isbn, $quantity) {
+    // Implementation here
+}
+
+function removeBook($isbn) {
+    // Implementation here
+}
+
+function listBooks() {
+    // Implementation here
+}
+?>
